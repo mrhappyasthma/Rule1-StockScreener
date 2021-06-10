b268d2e9b5a5f58d8775136060dd41a9594384b7
MrHappyAsthma <mrhappyasthma@gmail.com>
2021-06-09 23:18:17 -0700

    Add a simple SQL script to fetch stocks below MoS price

---
+++ sql/stocks_currently_below_margin_of_safety.sql
@@ -0,0 +1,7 @@
+# A simple script to display all stocks whose current price is lower than the margin of safety price.
+#
+# NOTE: This script does not check that these stocks meet all of the Rule #1 criteria. It's just a
+#        simple screener.
+
+SELECT stocks.name, stocks.margin_of_safety_price, stocks.current_price FROM isthisstockgood.stocks
+WHERE stocks.current_price <= stocks.margin_of_safety_price;
