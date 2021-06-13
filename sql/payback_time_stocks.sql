61dd34ebbcee8be574a78e7ee9b85dfe8990a7b8
MrHappyAsthma <mrhappyasthma@gmail.com>
2021-06-12 20:37:17 -0700

    Add a query to filter payback_time eligible stocks.

---
+++ sql/payback_time_stocks.sql
@@ -0,0 +1,7 @@
+# A simple script to screen for stocks that meet the Payback Time requirements.
+#
+# Creates four tables, one for all stocks ==10, <10. One for <8. Another for <6.
+
+SELECT stocks.name, stocks.payback_time FROM isthisstockgood.stocks
+WHERE stocks.payback_time  > 0 and stocks.payback_time <= 10
+ORDER BY stocks.payback_time;
